e68f6ec23f346cd28694c7deb77d26872f76f364
Curtis <yamahabob@github.com>
2013-04-04 17:35:55 -0400

    updates2

---
+++ event_queue.cpp
@@ -0,0 +1,9 @@
+//
+//  event_queue.cpp
+//  MINIFTP
+//
+//  Created by Curtis Taylor on 4/4/13.
+//  Copyright (c) 2013 WPI. All rights reserved.
+//
+
+#include "event_queue.h"
---
+++ event_queue.h
@@ -0,0 +1,14 @@
+//
+//  event_queue.h
+//  MINIFTP
+//
+//  Created by Curtis Taylor on 4/4/13.
+//  Copyright (c) 2013 WPI. All rights reserved.
+//
+
+#ifndef __MINIFTP__event_queue__
+#define __MINIFTP__event_queue__
+
+#include <iostream>
+
+#endif /* defined(__MINIFTP__event_queue__) */
