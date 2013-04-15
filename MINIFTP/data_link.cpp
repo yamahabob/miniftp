d1ab37dd317e96230997e47e1b0c71280a938c33
Curtis <yamahabob@github.com>
2013-04-15 17:39:29 -0400

    all

--- MINIFTP/data_link.cpp
+++ MINIFTP/data_link.cpp
@@ -207,7 +207,6 @@ void wait_for_event(event_type *event, int sock){ // dl_die!!
     printQueue(queueHead);
 
     if(queueHead!=NULL){
-
         if((FRAME_TIMEOUT-(curTime-queueHead->timestamp))<1){
             //remove_byTime(queueHead, &queueHead, curTime);
             *event=timeout;
@@ -216,15 +215,13 @@ void wait_for_event(event_type *event, int sock){ // dl_die!!
         else{
             timeToWait->tv_sec=FRAME_TIMEOUT-(curTime-queueHead->timestamp);
         }
-        
-        if(timeToWait->tv_sec<=0){
-            free(timeToWait);
-            timeToWait=NULL;
-            *event=timeout;
-            return;
-        }
     }
-        
+    
+    if(timeToWait->tv_sec<=0){
+        free(timeToWait);
+        timeToWait=NULL;
+    }
+    
     cout << "End of waitforevent\n";
     printQueue(queueHead);
     
